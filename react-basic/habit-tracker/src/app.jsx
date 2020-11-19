756dfc533fe26edc2988a61718b42b9d210c3fe2
Kim <fkqoseka@gmail.com>
2020-11-19 21:36:54 +0900

    jsx

--- react-basic/habit-tracker/src/app.jsx
+++ react-basic/habit-tracker/src/app.jsx
@@ -1,7 +1,17 @@
 import './app.css';
 
 function App() {
-  return <h1>Hello :)</h1>
+  const name = "";
+  return (
+    <> 
+      <h1>Hello</h1>
+      {name && <h1>Hello! {name}:) </h1>}
+      {['🍎', '🍓'].map(item => (
+        <h1>{item}</h1>
+      ))}
+ 
+    </>
+  );
 }
 
 export default App;
